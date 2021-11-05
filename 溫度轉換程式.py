2880cec219263e229c3e8859a8fb47cc8a985f8b
Ken Kuo <csco10898763@gmail.com>
2021-11-05 16:08:36 +0800

    add new file

--- 溫度轉換程式.py
+++ 溫度轉換程式.py
@@ -1,3 +1,15 @@
+# echo "# c_to_f" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/csco10898763/c_to_f.git
+# git push -u origin main
+
+# git remote add origin https://github.com/csco10898763/c_to_f.git
+# git branch -M main
+# git push -u origin main
+
 temp_C = input('請輸入攝氏溫度：')
 temp_C = float(temp_C)
 temp_F = temp_C * 9 / 5 + 32
